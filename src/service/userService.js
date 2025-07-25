ee4c8abf5a7ed495dcb549b86db17ba4260b46f9
Le Son <levanson2004dn@gmail.com>
2025-07-26 06:52:06 +0700

    6.1 orm

--- src/service/userService.js
+++ src/service/userService.js
@@ -16,22 +16,22 @@ const createNewUser=async(email,password,username)=>{
         database: 'jwt',
         port: '3307', Promise: bluebird
     });
-    connection.query('INSERT INTO users (email,password,username)VALUES (?,?,?)',[email,hashPass,username], 
+    connection.query('INSERT INTO user (email,password,username)VALUES (?,?,?)',[email,hashPass,username], 
         (err, rows, fields) => {
             if (err) throw err
         }
     )
 }
 const getListUser=async()=>{
-    let users =[];
-    // connection.query('select * from users ', 
+    let user =[];
+    // connection.query('select * from user ', 
     //      (err, rows, fields) => {
     //         if(err){
     //             console.log(err)
-    //             return users;
+    //             return user;
     //         }
-    //         users=rows;
-    //         return users;
+    //         user=rows;
+    //         return user;
     //     }
     // )
     const connection = await mysql.createConnection({
@@ -41,15 +41,15 @@ const getListUser=async()=>{
         port: '3307', Promise: bluebird
     });
     try{
-    const [rows, fields] = await connection.execute('SELECT * FROM users');
-    users=rows;
+    const [rows, fields] = await connection.execute('SELECT * FROM user');
+    user=rows;
 
     }catch{
         (err)=>{
             console.log(err)
         }
     }
-    return users;
+    return user;
 }
 const deleteUser=async(id)=>{
     const connection = await mysql.createConnection({
@@ -59,7 +59,7 @@ const deleteUser=async(id)=>{
         port: '3307', Promise: bluebird
     });
     try{
-    const [rows, fields] = await connection.execute('DELETE FROM users where id=?',[id]);
+    const [rows, fields] = await connection.execute('DELETE FROM user where id=?',[id]);
     return rows;
     }catch{
         (err)=>{
@@ -76,7 +76,7 @@ const getUserUpdate=async(id)=>{
         port: '3307', Promise: bluebird
     });
     try{
-    const [rows, fields] = await connection.execute('SELECT * FROM users where id=?',[id]);
+    const [rows, fields] = await connection.execute('SELECT * FROM user where id=?',[id]);
     userUpdate= rows[0];
     }catch(err){     
     console.log(err)
@@ -90,7 +90,7 @@ const updateUser=async(id,email,username)=>{
         database: 'jwt',
         port: '3307', Promise: bluebird
     });
-    await connection.execute("UPDATE users SET email = ?, username = ? WHERE id = ?",[email, username, id]);
+    await connection.execute("UPDATE user SET email = ?, username = ? WHERE id = ?",[email, username, id]);
 }
 module.exports={
     createNewUser,getListUser,deleteUser,getUserUpdate,updateUser
--- src/sever.js
+++ src/sever.js
@@ -1,12 +1,15 @@
 const express = require('express')
 require('dotenv').config()
 
-import initWebRouters from './routes/web'
-import configViewEngine from './config/viewengine'
+import initWebRouters from './routes/web';
+import configViewEngine from './config/viewengine';
+import connection from './config/connectDB';
 
 const port = process.env.PORT;
-const app = express()
+const app = express();
+
 configViewEngine(app);
+connection();
 initWebRouters(app);
 //body 
 
